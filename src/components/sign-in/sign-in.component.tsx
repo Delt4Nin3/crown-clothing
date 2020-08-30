a1663e9a3299a71a0dd38a9741dc9cfcf6ed1d03
Alexander Anding <alexander.anding@gmail.com>
2020-08-30 11:31:49 +0200

    make this a  button

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -40,7 +40,7 @@ class SignIn extends React.Component<any, State> {
                    label='Password' required/>
         <div className={'buttons'}>
           <CustomButton type={'submit'}>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+          <CustomButton type={'button'} onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
         </div>
       </form>
     </div>
