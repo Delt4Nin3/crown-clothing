6c000c23b8b3190a9f27d30bedc45070542e0bf6
Alexander Anding <alexander.anding@gmail.com>
2020-08-30 10:56:17 +0200

    add style to sign in /sign up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.page.tsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SignIn from "components/sign-in/sign-in.component";
+import SignUp from "components/sign-up/sign-up.component";
+
+import './sign-in-and-sign-up.styles.scss';
 
 class SignInAndSignUpPage extends React.Component<any, any> {
   render() {
     return <div className={'sign-in-and-sign-up'}>
       <SignIn/>
+      <SignUp/>
     </div>;
   }
 }
