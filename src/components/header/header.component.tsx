d8d43f689e1629d0fc16d0c3ef23ed1eb04cfd64
Alexander Anding <alexander.anding@gmail.com>
2020-08-31 19:44:00 +0200

    add cart basics

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -4,6 +4,8 @@ import { ReactComponent as Logo } from "assets/crown.svg";
 import { auth } from 'firebase/firebase.utils';
 import './header.styles.scss';
 import { connect } from "react-redux";
+import CartIcon from "components/cart-icon/cart-icon.component";
+import CartDropdown from "components/cart-dropdown/cart-dropdown.component";
 
 interface User {
   id: string
@@ -36,7 +38,9 @@ class Header extends React.Component<Props, any> {
             <Link className={'option'} to={'/signIn'}>SIGN IN</Link>
           )
         }
+        <CartIcon/>
       </div>
+      <CartDropdown/>
     </div>
   }
 }
