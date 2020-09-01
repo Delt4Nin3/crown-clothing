059b550fcf51487c67d2e86e5cba53664d036f42
Alexander Anding <alexander.anding@gmail.com>
2020-09-01 21:51:29 +0200

    try that fancy serviceWorker 🚀

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import * as serviceWorker from 'serviceWorker';
 
 import store from './redux/store';
 
@@ -17,3 +18,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
