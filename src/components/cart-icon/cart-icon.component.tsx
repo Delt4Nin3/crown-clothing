97a252ebbacfb489647384c83aa863b280e55c75
Alexander Anding <alexander.anding@gmail.com>
2020-09-02 21:55:47 +0200

    make it display the count.

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -3,12 +3,13 @@ import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from "assets/shopping-bag.svg";
 import { connect } from 'react-redux'
 import { toggleCartHidden } from 'redux/cart';
+import { CartItem } from "../../interfaces";
 
 class CartIcon extends React.Component<any, any> {
   render() {
     return <div className={'cart-icon'} onClick={this.props.toggleCartHidden}>
       <ShoppingIcon className={'shopping-icon'}/>
-      <span className={'item-count'}>0</span>
+      <span className={'item-count'}>{this.props.itemCount}</span>
     </div>;
   }
 }
@@ -17,4 +18,10 @@ const mapDispatchToProps = (dispatch: any) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state: any) => ({
+  itemCount: state.cart.cartItems.reduce((accumulatedQuantity: number, cartItem: CartItem) => {
+    return accumulatedQuantity + cartItem.quantity
+  }, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
