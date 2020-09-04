ea1950b32adecb004b51ad91fe16006d4d9ff224
Alexander Anding <alexander.anding@gmail.com>
2020-09-04 21:05:34 +0200

    implement redux persist

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
 import { userReducer } from './user';
 import { cartReducer } from "./cart";
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteSpace: ['cart'],
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
